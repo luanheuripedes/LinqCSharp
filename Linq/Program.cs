9f406eb0f9edaa08d974e1c428b8602c12288be3
luanheuripedes <luanheuripedesmoreiradepaula@gmail.com>
2023-07-04 21:18:26 -0300

    Filtragem

--- Linq/Program.cs
+++ Linq/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Linq.Persistence;
+
+var dbContext = new WorkDbContext();
+
+var employees = dbContext.Employees;
+
+// Filtragem
+var itEmployees = employees.Where(e => e.BusinessArea == "TI").ToList();
+var over10000SalaryEmployees = employees.Where(e => e.Salary >= 10_000).ToList();
+var nonInvoiceEmployees = employees.Where(e => !e.Invoices.Any()).ToList();
+
+// Busca
+
+// Projeção
+
+
+Console.ReadLine();
